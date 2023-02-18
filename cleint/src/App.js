a11b0a89137e58a463af58d63596a884c0429cf2
gitirom <wadiaromdane10@gmail.com>
2023-02-18 12:08:13 +0100

    creating routes and components

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -1,11 +1,53 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import all components
+import Register from "./components/Register";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import PageNotFound from "./components/PageNotFound";
+import Reset from "./components/Reset";
+import UserName from "./components/UserName";
 
 
-
+// root routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <UserName  />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/Password',
+    element: <Password />
+  },
+  
+  {
+    path: '/Profile',
+    element: <Profile />
+  },
+  {
+    path: '/Recovery',
+    element: <Recovery />
+  },
+  {
+    path: '/Reset',
+    element: <Reset />
+  },
+  {
+    path: '/*',
+    element: <PageNotFound />
+  },
+  
+]);
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
