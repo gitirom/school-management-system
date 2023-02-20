1c5ba1a84fdf40026bc0c702e01e1ced1b942825
gitirom <wadiaromdane10@gmail.com>
2023-02-20 22:09:40 +0100

    add some conditions for the sing up form .

--- cleint/src/components/Register.jsx
+++ cleint/src/components/Register.jsx
@@ -4,8 +4,55 @@ import { NavLink } from "react-router-dom";
 import "./mix.css";
 
 const Register = () => {
-    const [passShow, setpassShow] = useState(false);
-    const [cpassShow, setCpassShow] = useState(false);
+    const [passShow, setPassShow] = useState(false);
+    const [cpassShow, setCPassShow] = useState(false);
+    
+    
+    const [inpval, setinpval] = useState({
+        name: "",
+        email: "",
+        password: "",
+        cpassword:""
+    });
+
+    const setVal = (e) => {
+        //console.log(e.target.value);
+        const {name, value} = e.target;
+        setinpval(() => {
+            return {
+                ...inpval,
+                [name]:value                             // this function setVal take the setInpVal function and return all the object inpVal name and value
+            }
+        })
+    };
+
+    const addUserData = (e) => {
+        e.preventDefault();                                // for get no refresh the page when you hit the submit button
+        
+        const {name,email,password,cpassword} = inpval;    // destructing here
+
+        if(name === ""){
+            alert("Please Enter Your Name");
+        }else if(email === ""){
+            alert("Please Enter Your Email");
+        }else if(!email.includes("@")){
+            alert(" Enter A Valid Email");
+        }else if(password === ""){
+            alert("Please Enter Your password");
+        }else if(password.length < 6){
+            alert("password must be 6 char");
+        }else if(cpassword === ""){
+            alert("enter your confirm Password");
+        }else if(cpassword.length < 6){
+            alert("password must be 6 char");
+        }else if(password !== cpassword){
+            alert("password and confirm password not match");
+        }else{
+            console.log("User Registration successfully Done");
+        }
+    
+    }
+
 return (
     <>
     <section>
@@ -20,17 +67,17 @@ return (
             <form>
                         <div className="form_input">
                             <label htmlFor="text">Name</label>
-                            <input type="text" name="name" id="name" placeholder='Enter Your Name ' />
+                            <input type="text" onChange={setVal} value={inpval.name} name="name" id="name" placeholder='Enter Your Name ' />
                         </div>
                         <div className="form_input">
                             <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" placeholder='Enter Your Email Address' />
+                            <input type="email" onChange={setVal} value={inpval.email} name="email" id="email" placeholder='Enter Your Email Address' />
                         </div>
                         <div className="form_input">
                             <label htmlFor="password">Password</label>
                             <div className="two">
-                                <input type={!passShow ? "password" : "text"} name="password" id="password" placeholder='Enter Your password' />
-                                <div className="showpass" onClick={() => setpassShow(!passShow)} >
+                                <input type={!passShow ? "password" : "text"} onChange={setVal} value={inpval.password} name="password" id="password" placeholder='Enter Your password' />
+                                <div className="showpass" onClick={() => setPassShow(!passShow)} >
                                     {!passShow ? "Show" : "Hide"}                    
                                 </div>
                             </div>
@@ -39,14 +86,14 @@ return (
                         <div className="form_input">
                             <label htmlFor="password">Confirm Password</label>
                             <div className="two">
-                                <input type={!cpassShow ? "password" : "text"} name="cpassword" id="cpassword" placeholder='Confirm password' />
-                                <div className="showpass" onClick={() => setCpassShow(!cpassShow)} >
+                                <input type={!cpassShow ? "password" : "text"} onChange={setVal} value={inpval.cpassword}  name="cpassword" id="cpassword" placeholder='Confirm password' />
+                                <div className="showpass" onClick={() => setCPassShow(!cpassShow)} >
                                     {!cpassShow ? "Show" : "Hide"}                    
                                 </div>
                             </div>
                             
                         </div>
-                        <button className='btn' >Sign Up</button>
+                        <button className='btn' onClick={addUserData}>Sign Up</button>
                         <p>Already have an Account? <NavLink to="/">Log In</NavLink> </p>
             </form>
         </div>
