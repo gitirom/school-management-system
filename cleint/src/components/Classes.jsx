75bba52beee43e919246d4a80e284882245f2f94
gitirom <wadiaromdane10@gmail.com>
2023-03-09 12:16:14 +0100

    store jwt token in local storage, pass token in Authorization header

--- cleint/src/components/Classes.jsx
+++ cleint/src/components/Classes.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+
+import React,{useEffect} from 'react';
 
 const Classes = () => {
+    const ClasseValid = async () =>{
+        // store jwt token in local storage
+
+        let token = localStorage.getItem("usersdatatoken");
+
+        //pass token in Authorization header
+        const res = await fetch("/validUser", {
+            method:"GET",
+            headers:{
+                "content-type":"application/json",
+                "Authorization":token
+            }
+        });
+
+        const data =  await res.json();
+        console.log(data);
+    }
+    
+    useEffect(() => {
+        ClasseValid();
+    }, []);
+
     return (
         <>
             <div>Classes Page here</div>
