a95fc9f4ad34835b96adf1063b65db494c6c8966
gitirom <wadiaromdane10@gmail.com>
2023-05-24 15:15:41 +0200

    I complete the Serach and Delete API's

--- server/controllers/images.controllers.js
+++ server/controllers/images.controllers.js
@@ -2,30 +2,70 @@ const multer = require('multer');
 const upload = require('../util/Upload');
 const uploadImage = upload.single('image');
 const Image = require('../models/image.model');
+const ValidateImage = require('../validation/image');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     Add: async(req, res) => {
-        uploadImage(req, res, async function(err){
-            if (err) {
-                res.status(404).json({image: err.message})
-            }else{
-                // upload the image in DB
-                const image = {
-                    title: req.body.title,
-                    image: req.file.filename,
-                    path: "http://localhost:8009" + "/images/" + req.file.filename
+        try {
+            uploadImage(req, res, async function(err){
+                const {errors, isValid} = ValidateImage(req)
+                if (err) {
+                    errors.image = err.message
+                    return res.status(404).json(errors)
+                }else{
+                    // upload the image in DB
+                    if (!isValid) {
+                        return res.status(404).json(errors)
+                    } else {
+                        const image = {
+                            title: req.body.title,
+                            image: req.file.filename,
+                            path: "http://localhost:8009" + "/images/" + req.file.filename
+                        }
+                        await Image.create(image)
+                        await Image.find().then(result => {
+                            res.status(200).json(result)
+                        })
+                    }
                 }
-                await Image.create(image)
-                await Image.find().then(result => {
-                    res.status(200).json(result)
-                })
-            }
-        })
+            });
+        } catch (error) {
+            res.status(500).json({message: err.message})
+        }
     },
-    FindAll: async(req, res) => {
-        res.send("work");
+
+    FindAll: async(req, res) => {     //this API for get All the Images from db 
+        try {
+            await Image.find()
+            .then(result => {
+                res.status(200).json(result)
+            })
+        } catch (error) {
+            res.status(500).json({message: err.message})
+        }
     },
+    
     Delete: async(req, res) => {
-        res.send("work");
-    }
-}
+        try {
+            const image = await Image.findOne({ _id: req.params.id });                 //for getting the image ID 
+            const link = path.join(                                                //for get the complete image path 
+            __dirname,
+            "../public",
+            image.path.split("http://localhost:8009")[1].toString()
+        );
+        await fs.unlink(link, async(err) => {     //delete the image from the disck first  
+            await Image.findByIdAndRemove({_id: req.params.id})      //then from the DB
+            .then(async()=> {
+                await Image.findOne()
+                .then(data => {
+                    res.status(200).json(data)
+                })
+            })
+        })
+        } catch (error) {
+            res.status(500).json({message: err.message});
+        }
+    },
+};
---
+++ server/validation/image.js
@@ -0,0 +1,19 @@
+const validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = function ValidateImage(req) {
+    let errors = {};
+    req.body.title = !isEmpty(req.body.title) ? req.body.title : "";
+    req.file = !isEmpty(req.file) ? req.file : "";
+
+    if(validator.isEmpty(req.body.title)){
+        errors.title = "Title field is required";
+    }
+    if(!req.file.filename){
+        errors.image = "image is required";
+    }
+    return {
+        errors,
+        isValid: isEmpty(errors),
+    };
+};
